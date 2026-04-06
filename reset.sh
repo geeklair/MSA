a9fb394bf501ef39419f9e2c177526668287f798
Pete Beckman <beckman@anl.gov>
2026-04-06 15:12:30 -0500

    removed old file

--- reset.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-# This script has moved to bin/reset.sh.
-# It now supports --clean and --template flags and reads from scratchpads/active.*.yaml templates.
-exec "$(dirname "$0")/bin/reset.sh" "$@"
